2ecb797a4211148a518024b5d99a997be58599d1
sambellekaplan <kaplansb@dukes.jmu.edu>
2026-06-25 21:16:41 +0530

    testtrigger13

--- app.py
+++ app.py
@@ -196,3 +196,6 @@ if __name__ == '__main__':
     print("  Slow endpoint: http://localhost:5000/slow")
     print("=" * 55)
     app.run(host='0.0.0.0', port=5000, debug=False)
+
+
+# test piprline trigger
